3c34ced892f832720a034a40560d6da5cfdbb00f
Masood Mawlavizada <masmawlav@ucdavis.edu>
2017-06-20 15:37:41 -0700

    1.3 | Index HTML Setup

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-var app = angular.module('store', []);
+(function() { //This is called a closure, a good habit to get into.
+	var app = angular.module('store', []);
+
+	app.controller('StoreController', function(){
+		this.product = gem;
+	});
+
+	var gem = {
+		name: "Diamond",
+		price: "299.95",
+		description: "This is one of the most expensive gems you can buy"
+	}
+
+})();
